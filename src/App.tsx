824080c6c7626111ec15364067ffffb8debfb41a
megl1710 <maregandarela@gmail.com>
2025-06-16 17:48:30 -0600

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { ChakraProvider, Box } from '@chakra-ui/react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Router>
+        <Box minH="100vh" bg="gray.50">
+          <Navbar />
+          <Box maxW="1200px" mx="auto" px={4} py={8}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Box>
+        </Box>
+      </Router>
+    </ChakraProvider>
+  )
+}
+
+export default App
